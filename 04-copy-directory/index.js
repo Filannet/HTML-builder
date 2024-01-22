fb865fb4200b7b85c29616af2685ee6893a964c5
Anna Filatova <filanna33@gmail.com>
2024-01-22 20:56:12 +0400

    feat: copy directory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const pathToOriginDir = path.join(__dirname, '/files');
+const pathToCopiedDir = path.join(__dirname, '/files-copy');
+
+fs.mkdir(pathToCopiedDir, { recursive: true }, (err) => {
+  if (err) console.log(err);
+  else {
+    fs.readdir(pathToCopiedDir, (err, files) => {
+      files.forEach((file) => {
+        fs.unlink(path.join(pathToCopiedDir, file), () => {});
+      });
+
+      fs.readdir(pathToOriginDir, (err, files) => {
+        files.forEach((file) => {
+          const pathToOriginFile = path.join(pathToOriginDir, file);
+          const pathToCopiedFile = path.join(pathToCopiedDir, file);
+          fs.copyFile(pathToOriginFile, pathToCopiedFile, (err) => {
+            if (err) console.error(err);
+          });
+        });
+      });
+    });
+  }
+});
