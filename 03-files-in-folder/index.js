d6e87cf49afd96e57e092ff107b4c4cf4a85a260
Anna Filatova <filanna33@gmail.com>
2024-01-22 05:32:56 +0400

    feat: add displaying information about files stored in a folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const pathToDir = path.join(__dirname, '/secret-folder');
+
+fs.readdir(pathToDir, (err, files) => {
+  files.forEach((file) => {
+    const pathToFile = path.join(pathToDir, file);
+    fs.stat(pathToFile, (err, stats) => {
+      if (!stats.isDirectory()) {
+        const [name, ext] = file.split('.');
+        console.log(`${name} - ${ext} - ${stats.size} bytes`);
+      }
+    });
+  });
+});
